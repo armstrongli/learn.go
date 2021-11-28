4e3b212ea19b4ea917804cdbe40880c88a35dbdc
jianqli <jianqli@ebay.com>
2021-11-28 19:57:13 +0800

    变量不能做类型转换

--- 01.hello/main.go
+++ 01.hello/main.go
@@ -5,10 +5,14 @@ import (
 )
 
 func main() {
-	var hello string = "hello , golang!"
-	fmt.Println(hello)
+	var name string = "小强"
+	fmt.Println(name)
 	var age int = 35
 	fmt.Println(age)
 	var tall float64 = 1.70
 	fmt.Println(tall)
+
+	name = "小强测试变量"
+	// age = "测试一下类型" // 类型不兼容报错
+	// tall = age // 类型不兼容错误
 }
