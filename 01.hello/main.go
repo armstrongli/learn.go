cdb5c6f6b02e622198b23e68c83f987170556ccc
jianqli <jianqli@ebay.com>
2021-11-28 19:14:02 +0800

    添加变量定义

---
+++ 01.hello/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var hello string = "hello , golang!"
+	fmt.Println(hello)
+	var age int = 35
+	fmt.Println(age)
+	var tall float64 = 1.70
+	fmt.Println(tall)
+}
