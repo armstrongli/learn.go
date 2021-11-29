179af5733a517e3bebe33089976e99c403853a99
jianqli <jianqli@ebay.com>
2021-11-29 10:06:47 +0800

    字符串格式化输出

---
+++ 03.strfmt/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("中文，abc，1234，🧡")
+	fmt.Printf("我的名字是%s\n", "小强")
+	fmt.Printf("我的名字是%q\n", "小强")
+	fmt.Printf("我的名字是%x\n", "小强")
+	fmt.Printf("我的名字是%X\n", "小强")
+}
