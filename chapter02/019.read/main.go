5b0ba0f9c67cf403f8e1a838d400fef25f44e88d
jianqli <jianqli@ebay.com>
2021-12-17 09:48:51 +0800

    使用第三方包

--- chapter02/019.read/main.go
+++ chapter02/019.read/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"os"
 
-	_ "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
+	learn_go_tools "learn.go.tools"
+	calculator "learn.go/chapter02/015.fatrate.refactor/calc"
 )
 
 func main() {
@@ -12,26 +13,35 @@ func main() {
 	var (
 		name   string
 		sex    string
-		tall   string // float64
-		weight string // float64
-		age    string // int
+		tall   float64
+		weight float64
+		age    int
 	)
-	arguments := os.Args
-	fmt.Println(arguments)
 
-	name = arguments[1]
-	sex = arguments[2]
-	tall = arguments[3]
-	weight = arguments[4]
-	age = arguments[5]
+	cmd := &cobra.Command{
+		Use:   "healthcheck",
+		Short: "体脂计算器，根据身高、体重、性别、年龄计算体脂比，并给出健康建议",
+		Long:  "该体脂计算器是基于BMI的体脂计算器。。。。。。。",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("name: ", name)
+			fmt.Println("sex: ", sex)
+			fmt.Println("tall: ", tall)
+			fmt.Println("weight: ", weight)
+			fmt.Println("age: ", age)
+			// 计算
+			bmi := calculator.CalcBMI(tall, weight)
+			fatRate := calculator.CalcFatRate(bmi, age, sex)
+			fmt.Println("fatRate: ", fatRate)
 
-	fmt.Println("name: ", name)
-	fmt.Println("sex: ", sex)
-	fmt.Println("tall: ", tall)
-	fmt.Println("weight: ", weight)
-	fmt.Println("age: ", age)
+			// 评估结果
+			fmt.Println(learn_go_tools.Max(3, 5))
+		},
+	}
+	cmd.Flags().StringVar(&name, "name", "", "姓名")
+	cmd.Flags().StringVar(&sex, "sex", "", "性别")
+	cmd.Flags().Float64Var(&tall, "tall", 0, "身高")
+	cmd.Flags().Float64Var(&weight, "weight", 0, "体重")
+	cmd.Flags().IntVar(&age, "age", 0, "年龄")
 
-	// 计算
-
-	// 评估结果
+	cmd.Execute()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module learn.go
 
 go 1.16
 
-require github.com/spf13/cobra v1.3.0
+require (
+	github.com/spf13/cobra v1.3.0
+	learn.go.tools v0.0.0-00010101000000-000000000000
+)
+
+replace learn.go.tools => ../learn.go.tools
+
+replace github.com/spf13/cobra => github.com/spf13/cobra v1.2.0
