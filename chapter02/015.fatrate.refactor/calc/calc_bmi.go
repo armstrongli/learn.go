4f16d11df6efb5cb214520180b03f5ba13ea2553
jianqli <jianqli@ebay.com>
2021-12-20 10:24:55 +0800

    调整方法：return error，而不是直接panic

--- chapter02/015.fatrate.refactor/calc/calc_bmi.go
+++ chapter02/015.fatrate.refactor/calc/calc_bmi.go
@@ -1,9 +1,13 @@
 package calculator
 
-func CalcBMI(tall float64, weight float64) (bmi float64) {
+import (
+	"fmt"
+)
+
+func CalcBMI(tall float64, weight float64) (bmi float64, err error) {
 	if tall <= 0 {
-		panic("身高不能是0或者负数")
+		return 0, fmt.Errorf("身高不能是0或者负数")
 	}
 	// todo 验证体重的合法性
-	return weight / (tall * tall)
+	return weight / (tall * tall), nil
 }
