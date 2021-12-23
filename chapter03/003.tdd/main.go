42eb3f1a85a9cf54947bdadee0adeb32c90f471b
jianqli <jianqli@ebay.com>
2021-12-23 21:20:49 +0800

    完成案例2 的需求，修正代码

--- chapter03/003.tdd/main.go
+++ chapter03/003.tdd/main.go
@@ -13,19 +13,21 @@ func inputRecord(name string, fatRate float64) {
 }
 
 func getRand(name string) (rank int, fatRate float64) {
-	fatRate2PersonMap := map[float64]string{}
+	fatRate2PersonMap := map[float64][]string{}
 	rankArr := make([]float64, 0, len(personFatRate))
 	for nameItem, frItem := range personFatRate {
-		fatRate2PersonMap[frItem] = nameItem
+		fatRate2PersonMap[frItem] = append(fatRate2PersonMap[frItem], nameItem)
 		rankArr = append(rankArr, frItem)
 	}
 	sort.Float64s(rankArr)
 	for i, frItem := range rankArr {
-		_name := fatRate2PersonMap[frItem]
-		if _name == name {
-			rank = i + 1
-			fatRate = frItem
-			return
+		_names := fatRate2PersonMap[frItem]
+		for _, _name := range _names {
+			if _name == name {
+				rank = i + 1
+				fatRate = frItem
+				return
+			}
 		}
 	}
 	return 0, 0
