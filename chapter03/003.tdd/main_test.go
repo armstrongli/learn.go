d2d3c1a85f30dd268eeba2bf238d70d0adafed4b
jianqli <jianqli@ebay.com>
2021-12-23 20:54:56 +0800

    准备第一个测试案例： 案例1： 王强第一次录入的时候，他的体脂是 38 王强第二次录入的时候，他的体脂是 32 这时，王强的最佳体脂是 32 李静录入他的体脂 28 李静的最佳体脂是 28 李静排名第一，体脂 28；王强排名第二，体脂 32。

---
+++ chapter03/003.tdd/main_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCase1(t *testing.T) {
+	inputRecord("王强", 0.38)
+	inputRecord("王强", 0.32)
+	{
+		randOfWQ, fatRateOfWQ := getRand("王强")
+		if randOfWQ != 1 {
+			t.Fatalf("预期 王强 第一，但是得到的是：%d", randOfWQ)
+		}
+		if fatRateOfWQ != 0.32 {
+			t.Fatalf("预期 王强的体脂是 0.32，但得到的是：%f", fatRateOfWQ)
+		}
+	}
+	inputRecord("李静", 0.28)
+	{
+		randOfWQ, fatRateOfWQ := getRand("王强")
+		if randOfWQ != 2 {
+			t.Fatalf("预期 王强 第一，但是得到的是：%d", randOfWQ)
+		}
+		if fatRateOfWQ != 0.32 {
+			t.Fatalf("预期 王强的体脂是 0.32，但得到的是：%f", fatRateOfWQ)
+		}
+	}
+	{
+		randOfLJ, fatRateOfLJ := getRand("李静")
+		if randOfLJ != 1 {
+			t.Fatalf("预期 李静 第一，但是得到的是：%d", randOfLJ)
+		}
+		if fatRateOfLJ != 0.28 {
+			t.Fatalf("预期 李静的体脂是 0.28，但得到的是：%f", fatRateOfLJ)
+		}
+	}
+}
+
+func TestCase2(t *testing.T) {
+
+}
