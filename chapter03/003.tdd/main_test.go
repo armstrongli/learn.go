34bd859d79597ca663e0a41d52042914b69cf39c
jianqli <jianqli@ebay.com>
2021-12-23 21:16:11 +0800

    添加案例2 王强录入体脂是 38 张伟录入体脂是 38 李静录入体脂是 28 李静排名第一，体脂 28；王强、张伟排名第二，体脂 38。

--- chapter03/003.tdd/main_test.go
+++ chapter03/003.tdd/main_test.go
@@ -52,5 +52,35 @@ func TestCase1(t *testing.T) {
 }
 
 func TestCase2(t *testing.T) {
+	inputRecord("王强", 0.38)
+	inputRecord("张伟", 0.38)
+	inputRecord("李静", 0.28)
 
+	{
+		randOfLJ, fatRateOfLJ := getRand("李静")
+		if randOfLJ != 1 {
+			t.Fatalf("预期 李静 第一，但是得到的是：%d", randOfLJ)
+		}
+		if fatRateOfLJ != 0.28 {
+			t.Fatalf("预期 李静的体脂是 0.28，但得到的是：%f", fatRateOfLJ)
+		}
+	}
+	{
+		randOfWQ, fatRateOfWQ := getRand("王强")
+		if randOfWQ != 2 {
+			t.Fatalf("预期 王强 第一，但是得到的是：%d", randOfWQ)
+		}
+		if fatRateOfWQ != 0.38 {
+			t.Fatalf("预期 王强的体脂是 0.38，但得到的是：%f", fatRateOfWQ)
+		}
+	}
+	{
+		randOfZW, fatRateOfZW := getRand("张伟")
+		if randOfZW != 2 {
+			t.Fatalf("预期 张伟 第一，但是得到的是：%d", randOfZW)
+		}
+		if fatRateOfZW != 0.38 {
+			t.Fatalf("预期 张伟的体脂是 0.38，但得到的是：%f", fatRateOfZW)
+		}
+	}
 }
