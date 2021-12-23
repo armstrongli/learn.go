edc154f69420227a2618b8e15176963e4f513a2c
jianqli <jianqli@ebay.com>
2021-12-23 21:12:54 +0800

    完成案例1的代码实现

--- chapter03/003.tdd/main.go
+++ chapter03/003.tdd/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"sort"
+)
+
+var (
+	personFatRate = map[string]float64{}
+)
+
+func inputRecord(name string, fatRate float64) {
+	personFatRate[name] = fatRate
+}
+
+func getRand(name string) (rank int, fatRate float64) {
+	fatRate2PersonMap := map[float64]string{}
+	rankArr := make([]float64, 0, len(personFatRate))
+	for nameItem, frItem := range personFatRate {
+		fatRate2PersonMap[frItem] = nameItem
+		rankArr = append(rankArr, frItem)
+	}
+	sort.Float64s(rankArr)
+	for i, frItem := range rankArr {
+		_name := fatRate2PersonMap[frItem]
+		if _name == name {
+			rank = i + 1
+			fatRate = frItem
+			return
+		}
+	}
+	return 0, 0
+}
--- chapter03/003.tdd/main_test.go
+++ chapter03/003.tdd/main_test.go
@@ -4,6 +4,20 @@ import (
 	"testing"
 )
 
+func TestCase1Part1(t *testing.T) {
+	inputRecord("王强", 0.38)
+	inputRecord("王强", 0.32)
+	{
+		randOfWQ, fatRateOfWQ := getRand("王强")
+		if randOfWQ != 1 {
+			t.Fatalf("预期 王强 第一，但是得到的是：%d", randOfWQ)
+		}
+		if fatRateOfWQ != 0.32 {
+			t.Fatalf("预期 王强的体脂是 0.32，但得到的是：%f", fatRateOfWQ)
+		}
+	}
+}
+
 func TestCase1(t *testing.T) {
 	inputRecord("王强", 0.38)
 	inputRecord("王强", 0.32)
