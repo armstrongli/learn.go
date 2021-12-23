b0e08c7ab869c5b59540e619ab41af37e5a45018
jianqli <jianqli@ebay.com>
2021-12-23 21:23:38 +0800

    添加案例3 王强录入体脂是 38 李静录入体脂是 28 张伟注册成功，未录入体脂 李静排名第一，体脂 28；王强排名第二，体脂 38；张伟排名第三，没有体脂记录。

--- chapter03/003.tdd/main_test.go
+++ chapter03/003.tdd/main_test.go
@@ -68,7 +68,7 @@ func TestCase2(t *testing.T) {
 	{
 		randOfWQ, fatRateOfWQ := getRand("王强")
 		if randOfWQ != 2 {
-			t.Fatalf("预期 王强 第一，但是得到的是：%d", randOfWQ)
+			t.Fatalf("预期 王强 第2，但是得到的是：%d", randOfWQ)
 		}
 		if fatRateOfWQ != 0.38 {
 			t.Fatalf("预期 王强的体脂是 0.38，但得到的是：%f", fatRateOfWQ)
@@ -77,10 +77,41 @@ func TestCase2(t *testing.T) {
 	{
 		randOfZW, fatRateOfZW := getRand("张伟")
 		if randOfZW != 2 {
-			t.Fatalf("预期 张伟 第一，但是得到的是：%d", randOfZW)
+			t.Fatalf("预期 张伟 第2，但是得到的是：%d", randOfZW)
 		}
 		if fatRateOfZW != 0.38 {
 			t.Fatalf("预期 张伟的体脂是 0.38，但得到的是：%f", fatRateOfZW)
+		}
+	}
+}
+
+func TestCase3(t *testing.T) {
+	inputRecord("王强", 0.38)
+	inputRecord("李静", 0.28)
+	inputRecord("张伟")
+
+	{
+		randOfLJ, fatRateOfLJ := getRand("李静")
+		if randOfLJ != 1 {
+			t.Fatalf("预期 李静 第一，但是得到的是：%d", randOfLJ)
+		}
+		if fatRateOfLJ != 0.28 {
+			t.Fatalf("预期 李静的体脂是 0.28，但得到的是：%f", fatRateOfLJ)
+		}
+	}
+	{
+		randOfWQ, fatRateOfWQ := getRand("王强")
+		if randOfWQ != 2 {
+			t.Fatalf("预期 王强 第一，但是得到的是：%d", randOfWQ)
+		}
+		if fatRateOfWQ != 0.38 {
+			t.Fatalf("预期 王强的体脂是 0.38，但得到的是：%f", fatRateOfWQ)
+		}
+	}
+	{
+		randOfZW, _ := getRand("张伟")
+		if randOfZW != 3 {
+			t.Fatalf("预期 张伟 第三，但是得到的是：%d", randOfZW)
 		}
 	}
 }
