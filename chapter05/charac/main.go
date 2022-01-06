e22c1d5bc02dddb68d5a9c5397b9ac0a8917a1d5
jianqli <jianqli@ebay.com>
2022-01-06 22:25:05 +0800

    接口特性： * 必须实现所有的函数才算实现了接口 * 成员函数可以在成员对象上实现，也可以在对象指针上实现。在给接口变量赋值时，如果是混合定义，要用指针 * 接口可以嵌套（组合） * 接口在彼此间转换时，从小范围到大范围可以无缝转换；反过来转换会失败，错误类型为：未实现接口定义的方法

--- chapter05/charac/main.go
+++ chapter05/charac/main.go
@@ -10,11 +10,26 @@ func main() {
 	var elephant Elephant
 	fmt.Println(elephant.Name)
 
-	var ip *int      // 默认是nil
-	fmt.Println(*ip) // panic 空指针
+	// var ip *int      // 默认是nil
+	// fmt.Println(*ip) // panic 空指针
+	// m := map[string]string{} // map 可以直接初始化实体，但是接口不行
 
-	var putER PutElephantIntoRefrigerator         // 默认是nil
-	putER.OpenTheDoorOfRefrigerator(refrigerator) // panic 空指针
+	// var putER PutElephantIntoRefrigerator // 默认是nil
+	// putER = PutElephantIntoRefrigerator{}
+	// putER.OpenTheDoorOfRefrigerator(refrigerator) // panic 空指针
+
+	// **注意：如果某个对象的成员方法有的在对象上，有的在对象指针上，那么在给接口赋值时，必须用指针。**
+	var legend PutElephantIntoRefrigerator = &PutElephantIntoRefrigeratorImpl{}
+	legend.OpenTheDoorOfRefrigerator(refrigerator)
+	legend.PutElephantIntoRefrigerator(elephant, refrigerator)
+	legend.CloseTheDoorOfRefrigerator(refrigerator)
+
+	// var o Open = Refrigerator{}
+	var c Close = Refrigerator{}
+	var b Box = Refrigerator{}
+	fmt.Println(b, c)
+	c = b // 从范围小的向范围大的转会成功
+	// b = c // 从范围大的向范围小的转会失败
 }
 
 type PutElephantIntoRefrigerator interface {
@@ -23,8 +38,62 @@ type PutElephantIntoRefrigerator interface {
 	CloseTheDoorOfRefrigerator(Refrigerator) error
 }
 
+type TestTypeImplInterface func()
+
+func (t TestTypeImplInterface) OpenTheDoorOfRefrigerator(_ Refrigerator) error {
+	return nil
+}
+func (t TestTypeImplInterface) PutElephantIntoRefrigerator(_ Elephant, _ Refrigerator) error {
+	return nil
+}
+func (t TestTypeImplInterface) CloseTheDoorOfRefrigerator(_ Refrigerator) error {
+	return nil
+}
+
+type PutElephantIntoRefrigeratorImpl struct {
+}
+
+func (legent *PutElephantIntoRefrigeratorImpl) OpenTheDoorOfRefrigerator(refrigerator Refrigerator) error {
+	// todo
+	fmt.Println("打开冰箱门")
+	return nil
+}
+
+func (legent *PutElephantIntoRefrigeratorImpl) PutElephantIntoRefrigerator(elephant Elephant, refrigerator Refrigerator) error {
+	// todo
+	fmt.Println("装进去")
+	return nil
+}
+
+func (legent *PutElephantIntoRefrigeratorImpl) CloseTheDoorOfRefrigerator(refrigerator Refrigerator) error {
+	// todo
+	fmt.Println("关门")
+	return nil
+}
+
+type Open interface {
+	Open() error
+}
+
+type Close interface {
+	Close() error
+}
+
+type Box interface {
+	Open
+	Close
+}
+
 type Refrigerator struct {
 	Size string
+}
+
+func (Refrigerator) Open() error {
+	return nil
+}
+
+func (Refrigerator) Close() error {
+	return nil
 }
 
 type Elephant struct {
