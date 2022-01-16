5b4bb348a12740e81159c09804fa34c5969d1c4b
jianqli <jianqli@ebay.com>
2022-01-16 20:46:03 +0800

    添加起跑令

--- chapter06/06.sync.wg/waitgroup.go
+++ chapter06/06.sync.wg/waitgroup.go
@@ -11,8 +11,10 @@ type Runner struct {
 	Name string
 }
 
-func (r Runner) Run(wg *sync.WaitGroup) {
+func (r Runner) Run(startPointWg, wg *sync.WaitGroup) {
 	defer wg.Done()
+	startPointWg.Wait()
+
 	start := time.Now()
 	fmt.Println(r.Name, "开始跑@", start)
 	rand.Seed(time.Now().UnixNano())
@@ -28,6 +30,9 @@ func main() {
 	wg := sync.WaitGroup{}
 	wg.Add(runnerCount)
 
+	startPointWg := sync.WaitGroup{}
+	startPointWg.Add(1)
+
 	for i := 0; i < runnerCount; i++ {
 		runners = append(runners, Runner{
 			Name: fmt.Sprintf("%d", i),
@@ -35,8 +40,14 @@ func main() {
 	}
 
 	for _, runnerItem := range runners {
-		go runnerItem.Run(&wg)
+		go runnerItem.Run(&startPointWg, &wg)
 	}
+
+	fmt.Println("各就位")
+	time.Sleep(1 * time.Second)
+	fmt.Println("预备：跑")
+
+	startPointWg.Done()
 
 	wg.Wait()
 	fmt.Println("赛跑结束")
