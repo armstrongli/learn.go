3b1474409b635d075b938b6ced2b2abcf4d1bac8
jianqli <jianqli@ebay.com>
2022-01-16 22:10:50 +0800

    once：只做一次

--- chapter06/07.sync.once/once.go
+++ chapter06/07.sync.once/once.go
@@ -9,28 +9,48 @@ type rank struct {
 }
 
 var globalRank = &rank{}
-var globalRankInitialized bool = false
-var globalRankInitializedLock sync.Mutex
-
-func init() {
-	globalRank.standard = []string{"Asia"}
-}
+var once sync.Once = sync.Once{}
 
 func initGlobalRankStandard(standard []string) {
-	globalRankInitializedLock.Lock()
-	defer globalRankInitializedLock.Unlock()
-	if globalRankInitialized {
-		return
-	}
-	globalRank.standard = standard
-	globalRankInitialized = true
+	once.Do(func() {
+		globalRank.standard = standard
+	})
+}
+
+var facStore = &dbFactoryStore{}
+
+type dbFactoryStore struct {
+	store map[string]DBFactory
+}
+
+type Conn struct{}
+
+type DBFactory interface {
+	GetConnection() *Conn
+}
+
+func initMySqlFac(connStr string) DBFactory {
+	return &MySqlDBFactory{}
+}
+
+type MySqlDBFactory struct {
+	once sync.Once
+}
+
+func (MySqlDBFactory) GetConnection() *Conn {
+	once.Do(func() {
+		initMySqlFac("")
+	})
+	// todo
+	return nil
 }
 
 func main() {
-	standard := []string{"asia"}
-	for i := 0; i < 10; i++ {
-		go func() {
-			initGlobalRankStandard(standard)
-		}()
-	}
+	// standard := []string{"asia"}
+	// for i := 0; i < 10; i++ {
+	// 	go func() {
+	// 		initGlobalRankStandard(standard)
+	// 	}()
+	// }
+	// connStr := "xxxxxxxx"
 }
