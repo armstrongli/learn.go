dd96b16285ebd7e8b40e0dd0af0816eb0db9d932
jianqli <jianqli@ebay.com>
2022-01-16 22:10:06 +0800

    读写锁
    写锁只能一个goroutine拿到。读锁可以多个goroutine同时拿到。

--- chapter06/03.lock/main.go
+++ chapter06/03.lock/main.go
@@ -7,11 +7,12 @@ import (
 )
 
 func main() {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 1; i++ {
 		// countDict()
 		// countDictGoroutineSafe()
 		// countDictForgetUnlock()
-		countDictLockPrice()
+		// countDictLockPrice()
+		countDictGoroutineSafeRW()
 	}
 }
 
@@ -50,6 +51,45 @@ func countDictGoroutineSafe() {
 		}()
 	}
 	wg.Wait()
+	fmt.Println("预计有", 100*5000, "字")
+	fmt.Println("总共有：", totalCount, "字")
+}
+
+func countDictGoroutineSafeRW() {
+	fmt.Println("开始数")
+	var totalCount int64 = 0
+	totalCountLock := sync.RWMutex{}
+
+	wg := sync.WaitGroup{}
+	workerCount := 5
+	wg.Add(workerCount)
+
+	doneCh := make(chan struct{})
+	for p := 0; p < workerCount; p++ {
+		go func(p int) { // 读锁可以多个go routine同时拿到。
+			fmt.Println(p, "读锁开始时间：", time.Now())
+			totalCountLock.RLock()
+			fmt.Println(p, "读锁拿到锁时间：", time.Now())
+			time.Sleep(1 * time.Second)
+			totalCountLock.RUnlock()
+		}(p)
+	}
+	for p := 0; p < workerCount; p++ {
+		go func(p int) {
+			defer wg.Done()
+
+			fmt.Println(p, "写锁开始时间：", time.Now())
+			totalCountLock.Lock()
+			fmt.Println(p, "写锁拿到锁时间：", time.Now())
+			// fmt.Print("正在统计第", p, "页，")
+			defer totalCountLock.Unlock()
+			totalCount += 100 // totalCount = totalCount + 100 // 注意，这里有重复覆盖的问题
+			// totalCountLock.Unlock() // 也可以用defer来解锁
+		}(p)
+	}
+	wg.Wait()
+	close(doneCh)
+	time.Sleep(1 * time.Second)
 	fmt.Println("预计有", 100*5000, "字")
 	fmt.Println("总共有：", totalCount, "字")
 }
