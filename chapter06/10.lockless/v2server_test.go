2f2d72345179fd206bc3246772baf86e696c46fc
jianqli <jianqli@ebay.com>
2022-02-05 13:45:14 +0800

    调整request数量以显示出数据更新的次数

--- chapter06/10.lockless/v2server_test.go
+++ chapter06/10.lockless/v2server_test.go
@@ -13,18 +13,31 @@ func TestWebServerV2(t *testing.T) {
 	}
 	go v2.ReloadWorker()
 
+	m := sync.Map{}
+
 	start := time.Now()
+	concurrency := 100
 	wg := sync.WaitGroup{}
-	wg.Add(10000)
-	for i := 0; i < 10000; i++ {
+	wg.Add(concurrency)
+	for i := 0; i < concurrency; i++ {
 		go func() {
 			defer wg.Done()
-			for j := 0; j < 10000; j++ {
-				v2.Visit()
+			changed := 0
+			pre := v2.config.Content
+			for j := 0; j < 1000000000; j++ {
+				if v2.Visit() != pre {
+					changed++
+					pre = v2.config.Content
+				}
 			}
+			m.Store(time.Now().UnixNano(), changed)
 		}()
 	}
 	wg.Wait()
 	finish := time.Now()
 	fmt.Println("总时间：", finish.Sub(start))
+	m.Range(func(key, value interface{}) bool {
+		fmt.Println(key, value)
+		return true
+	})
 }
