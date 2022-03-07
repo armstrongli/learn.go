a4823ed6be35b08ba39675b04700e928bf244579
jianqli <jianqli@ebay.com>
2022-03-07 13:27:53 +0800

    hack sql 示例
    `--` 是 sql 语句中的注释，当sql中有未经处理的 -- 时，会将sql语句截断执行，从而hack到sql中窃取数据

--- chapter10/01.golang-connect-db/main.go
+++ chapter10/01.golang-connect-db/main.go
@@ -15,11 +15,13 @@ func main() {
 	defer learnDB.Close()
 	err = testDbConnection(err, learnDB)
 
-	queryAllData(err, learnDB)
+	queryAllDataWithHack(err, learnDB)
 
-	insertData(learnDB) // todo handle error
+	// queryAllData(err, learnDB)
 
-	queryAllData(err, learnDB)
+	// insertData(learnDB) // todo handle error
+
+	// queryAllData(err, learnDB)
 }
 
 func insertData(learnDB *sql.DB) error {
@@ -35,6 +37,34 @@ func insertData(learnDB *sql.DB) error {
 		return err
 	}
 	return nil
+}
+
+func queryAllDataWithHack(err error, learnDB *sql.DB) {
+	_sql := fmt.Sprintf(`select name,age from personal_info where name='%s' and sex = '%s'`, "小贝' -- ", "男")
+
+	rows, err := learnDB.Query(_sql)
+	// rows, err := learnDB.Query("select age,name from personal_info ") // 更换列的顺序，因为数据类型不匹配导致失败。
+	// 警告：如果不巧，数据类型兼容，会引入更大的灾难。
+	if err != nil {
+		log.Fatal(err)
+	}
+	list := &apis.PersonalInformationList{}
+	for rows.Next() {
+		var name string
+		var age int
+		if err := rows.Scan(&name, &age); err != nil {
+			// todo handle error
+			log.Printf("扫描数据失败，跳过该行: %v", err)
+			continue
+		}
+		fmt.Printf("name: %s\tage: %d\n", name, age)
+		list.Items = append(list.Items, &apis.PersonalInformation{
+			Name: name,
+			Age:  int64(age),
+		})
+	}
+	data, _ := json.Marshal(list)
+	fmt.Println(string(data))
 }
 
 func queryAllData(err error, learnDB *sql.DB) {
