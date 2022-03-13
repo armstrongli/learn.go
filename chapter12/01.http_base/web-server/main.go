faaf69c4c531da3e3263d07ce59c86465d5430ec
jianqli <jianqli@ebay.com>
2022-03-13 20:25:28 +0800

    从URL读取数据
    http://localhost:8088/?name=xiaoqiang&&sex=%E7%94%B7

--- chapter12/01.http_base/web-server/main.go
+++ chapter12/01.http_base/web-server/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
-	"time"
 )
 
 func main() {
 	http.ListenAndServe(":8088", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		time.Sleep(2 * time.Second)
-		writer.Write([]byte("hello,您好"))
+		qp := request.URL.Query()
+		data, _ := json.Marshal(qp)
+		writer.Write([]byte("hello,您好:" + string(data)))
 	}))
 }
