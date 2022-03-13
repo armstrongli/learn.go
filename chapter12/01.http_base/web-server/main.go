aa41f1e931a514b863db21046217add8b3fd8276
jianqli <jianqli@ebay.com>
2022-03-13 21:24:49 +0800

    使用servMux实现路由分发
    * 分发不同的请求
    * 实现不同的返回
    * 没有注册的路由，默认是404
    * switch/case来区分不同的method

--- chapter12/01.http_base/web-server/main.go
+++ chapter12/01.http_base/web-server/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	http.ListenAndServe(":8080", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		if request.Body == nil {
-			writer.Write([]byte("no body"))
-			return
-		}
-		// data, _ := ioutil.ReadAll(request.Body)
-		// defer request.Body.Close()
-		// encoded := base64.StdEncoding.EncodeToString(data)
-		writer.Write([]byte("append(data, []byte(encoded)...)"))
+	m := http.NewServeMux()
+	m.Handle("/hello", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte(`hello`))
 	}))
+	m.Handle("/rank", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte(`rank`))
+	}))
+	m.Handle("/history/xiaoqiang", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte(`xiaoqiang的黑历史`))
+	}))
+	m.Handle("/history/jesse", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte(`jesse的健身馆`))
+	}))
+	// .....
+	m.Handle("/history", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		qp := request.URL.Query()
+		name := qp.Get("name")
+		// if/else
+		// switch/case
+		// switch request.Method{
+		// case "get"://....
+		// case "post"://.....
+		// }
+		writer.WriteHeader(http.StatusOK)
+		writer.Write([]byte(fmt.Sprintf(`%s: %s的健身馆`, request.Method, name)))
+	}))
+	http.ListenAndServe(":8080", m)
 }
