aba046b07823bddb1320260fab2d8fb61ba7faa8
jianqli <jianqli@ebay.com>
2022-03-21 13:49:14 +0800

    添加命令行参数，使得能够通过命令行控制服务的入口

--- chapter01/00.cool/main.go
+++ chapter01/00.cool/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("/Users/jianqli/tmp")))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	var p string
+	flag.StringVar(&p, "path", ".", "the path to expose as http")
+	var port int
+	flag.IntVar(&port, "port", 8080, "the port to expose")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(p)))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
