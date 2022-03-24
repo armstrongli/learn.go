4f1059b77259deca8b4fdb95d6ae678a0cfdf0c2
jianqli <jianqli@ebay.com>
2022-03-24 22:18:17 +0800

    调用排行榜功能：注册、获取榜单

--- chapter13/02.grpc/client/grpc-client.go
+++ chapter13/02.grpc/client/grpc-client.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"log"
-	"time"
 
 	"google.golang.org/grpc"
 	"learn.go/pkg/apis"
@@ -16,32 +15,62 @@ func main() {
 		log.Fatalf("did not connect: %v", err)
 	}
 	c := apis.NewRankServiceClient(conn)
-	ret, err := c.Register(context.TODO(), &apis.PersonalInformation{Name: "tom"})
+	ret, err := c.Register(context.TODO(), &apis.PersonalInformation{
+		Name:   "tom",
+		Sex:    "男",
+		Tall:   1.7,
+		Weight: 70,
+		Age:    30,
+	})
 	if err != nil {
 		log.Fatal("注册失败：", err)
 	}
 	log.Println("注册成功", ret)
 
+	top, err := c.GetTop(context.TODO(), &apis.Null{})
+	if err != nil {
+		log.Fatal("获取榜单失败：", err)
+	}
+	log.Println("榜单：", top.String())
+
 	log.Println("开始批量注册")
 	regCli, err := c.RegisterPersons(context.TODO())
-	if err != nil {
-		log.Fatal("获取批量注册客户端失败：", err)
-	}
-	if err := regCli.Send(&apis.PersonalInformation{Name: fmt.Sprintf("tom-%d", time.Now().Nanosecond())}); err != nil {
+	if err := regCli.Send(&apis.PersonalInformation{
+		Name:   "tom-3",
+		Sex:    "男",
+		Tall:   1.7,
+		Weight: 70,
+		Age:    30,
+	}); err != nil {
 		log.Fatal("注册时失败：", err)
 	}
-	if err := regCli.Send(&apis.PersonalInformation{Name: fmt.Sprintf("tom-%d", time.Now().Nanosecond())}); err != nil {
+	if err := regCli.Send(&apis.PersonalInformation{
+		Name:   "tom-1",
+		Sex:    "男",
+		Tall:   1.7,
+		Weight: 70,
+		Age:    30,
+	}); err != nil {
 		log.Fatal("注册时失败：", err)
 	}
-	if err := regCli.Send(&apis.PersonalInformation{Name: fmt.Sprintf("tom-%d", time.Now().Nanosecond())}); err != nil {
+	if err := regCli.Send(&apis.PersonalInformation{
+		Name:   "tom-2",
+		Sex:    "男",
+		Tall:   1.7,
+		Weight: 70,
+		Age:    30,
+	}); err != nil {
 		log.Fatal("注册时失败：", err)
 	}
-	if err := regCli.Send(&apis.PersonalInformation{Name: fmt.Sprintf("tom-%d", time.Now().Nanosecond())}); err != nil {
-		log.Fatal("注册时失败：", err)
+	regCli.CloseAndRecv()
+
+	{
+		top, err := c.GetTop(context.TODO(), &apis.Null{})
+		if err != nil {
+			log.Fatal("获取榜单失败：", err)
+		}
+		data, _ := json.Marshal(top)
+		log.Println("榜单：", string(data))
 	}
-	resp, err := regCli.CloseAndRecv()
-	if err != nil {
-		log.Fatal("无法接收结果：", err)
-	}
-	log.Println("批量注册结果：", resp.String())
+
 }
